245c7733222e193bd7596eca9ff4f88dace5b39f
ambur <ambur@LAPTOP-BPJT3S4B>
2024-11-17 23:04:23 +0530

    added shreyash as a name by devloper shreyash

--- src/com/ajay/test/Tester.java
+++ src/com/ajay/test/Tester.java
@@ -5,7 +5,7 @@ import com.ajay.components.WishMessageGenerator;
 public class Tester {
 	public static void main(String[] args) {
 		WishMessageGenerator generator=new WishMessageGenerator();
-		generator.generate("ajay");
+		generator.generate("shreyash");
 	}
 
 }
